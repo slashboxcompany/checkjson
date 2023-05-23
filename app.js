48cae3cd46b7fbfa7d141ddff1789fd810385fe5
Akash chikara <akashchikara1998@gmail.com>
2023-05-23 21:29:46 +0530

    new features back button dialog

--- app.js
+++ app.js
@@ -47,3 +47,34 @@ const clearText = () => {
 document.getElementById("minify").addEventListener("click", () => formatText()),
 document.getElementById("beautify").addEventListener("click", () => formatText(4)),
 document.getElementById("clear").addEventListener("click", () => clearText());
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = "\o/";
+
+    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    return confirmationMessage;                            //Webkit, Safari, Chrome
+});
+window.onbeforeunload = function (e) {
+    var message = "Are you sure ?";
+    var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
+    if (firefox) {
+        //Add custom dialog
+        //Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
+        var dialog = document.createElement("div");
+        document.body.appendChild(dialog);
+        dialog.id = "dialog";
+        dialog.style.visibility = "hidden";
+        dialog.innerHTML = message;
+        var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
+        dialog.style.left = left + "px";
+        dialog.style.visibility = "visible";
+        var shadow = document.createElement("div");
+        document.body.appendChild(shadow);
+        shadow.id = "shadow";
+        //tip with setTimeout
+        setTimeout(function () {
+            document.body.removeChild(document.getElementById("dialog"));
+            document.body.removeChild(document.getElementById("shadow"));
+        }, 0);
+    }
+    return message;
+};
