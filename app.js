9fbc45776fbde4dc4b0418f149150c4e55efbe67
Vishal-sikchi-088 <151211879+Vishal-sikchi-088@users.noreply.github.com>
2023-11-18 04:22:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+class FixedSizeQueue {
+    constructor() {
+      this.items = [];
+      this.maxSize = 15;
+    }
+  
+    enqueue(item) {
+        const currentSize = this.size();
+    
+        if (currentSize < this.maxSize && this.items[currentSize - 1] !== item) {
+            this.items.push(item);
+        } else if (currentSize === this.maxSize && this.items[currentSize - 1] !== item) {
+            this.dequeue();
+            this.items.push(item);
+        }
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) {
+        return null;
+      }
+      return this.items.shift();
+    }
+  
+    showSelectedIndex(i) {
+      if (this.isEmpty()) {
+        return null;
+      }
+      return this.items[i];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  }
+const myQueue = new FixedSizeQueue();
+
+
+// Call the function to populate the sidebar
+
+
 const editor = ace.edit("editor", {
     mode: "ace/mode/json",
     selectionStyle: "text",
@@ -11,7 +56,7 @@ const editor = ace.edit("editor", {
     formatText = (e = 0) => {
         try {
             var t;
-            returnOriginalValue(), "" !== editor.getValue() ? (t = JSON.parse(editor.getValue()), editor.setValue(JSON.stringify(t,null, e)), editor.focus(), editor.selectAll(), 0 !== e && validState()) : alert("I think you are not clear about your goals.")
+            returnOriginalValue(), "" !== editor.getValue() ? (t = JSON.parse(editor.getValue()), (myQueue.enqueue(JSON.stringify(t,null, e))),populateSidebar(), editor.setValue(JSON.stringify(t,null, e)), editor.focus(), editor.selectAll(), 0 !== e && validState()) : alert("I think you are not clear about your goals.")
         } catch (e) {
             error()
         }
@@ -53,6 +98,63 @@ window.addEventListener("beforeunload", function (e) {
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage;                            //Webkit, Safari, Chrome
 });
+
+function populateSidebar() {
+    var historyList = document.getElementById("historyList");
+    historyList.innerHTML = "";
+    for (let i = 0; i < myQueue.size(); i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = myQueue.showSelectedIndex(i);
+        listItem.classList.add("history-item");
+        listItem.addEventListener("click", function () {
+            handleClick(myQueue.showSelectedIndex(i));
+        });
+        historyList.appendChild(listItem);
+    }
+}
+function handleClick(item) {
+    editor.setValue(JSON.stringify(JSON.parse(item)))
+    closeHistoryPanel()
+}
+
+function openHistoryPanel() {
+    var historyPanel = document.getElementById("historyPanel");
+    historyPanel.style.width = "350px";
+    historyPanel.classList.add("show");
+}
+
+function closeHistoryPanel() {
+    var historyPanel = document.getElementById("historyPanel");
+    historyPanel.style.width = "0";
+    historyPanel.classList.remove("show");
+}
+
+function toggleHistoryPanel(event) {
+    var historyPanel = document.getElementById("historyPanel");
+    
+    if (historyPanel.classList.contains("show")) {
+        closeHistoryPanel();
+    } else {
+        openHistoryPanel();
+    }
+
+    // Prevent the click event from propagating further (e.g., to the document click listener)
+    event.stopPropagation();
+}
+
+var historyButton = document.getElementById("history");
+historyButton.addEventListener("click", toggleHistoryPanel);
+
+document.addEventListener("click", function (event) {
+    var historyPanel = document.getElementById("historyPanel");
+
+    if (!historyPanel.contains(event.target) && historyPanel.classList.contains("show")) {
+        // Clicked outside the history panel, close it
+        closeHistoryPanel();
+    }
+});
+
+
 window.onbeforeunload = function (e) {
     var message = "Are you sure ?";
     var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
