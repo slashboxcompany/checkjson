52b3bfdba8449d452a980dd28a166c5033192e36
Akash chikara <56495692+ghasara@users.noreply.github.com>
2023-12-11 22:51:47 +0530

    Update app.js with indentation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class FixedSizeQueue {
     constructor() {
-        this.maxSize = 100
+        this.maxSize = 100;
         this.items = [];
         this.loadFromLocalStorage();
     }
@@ -8,16 +8,16 @@ class FixedSizeQueue {
     loadFromLocalStorage() {
         const savedQueue = localStorage.getItem('myQueue');
         this.items = savedQueue ? JSON.parse(savedQueue) : [];
-        this.setDeleteHistoryButton()
+        this.setDeleteHistoryButton();
     }
 
     enqueue(item) {
         const currentSize = this.size();
         if (this.items.length === 0 || (currentSize < this.maxSize && this.items[currentSize - 1].data !== item)) {
-            const object = { data : item, currentTime : new Date() }
+            const object = { data: item, currentTime: new Date() };
             this.items.push(object);
             this.saveToLocalStorage();
-            this.setDeleteHistoryButton()
+            this.setDeleteHistoryButton();
         } else if (currentSize === this.maxSize && this.items[currentSize - 1].data !== item) {
             this.dequeue();
             this.items.push(item);
@@ -26,18 +26,14 @@ class FixedSizeQueue {
     }
 
     setDeleteHistoryButton() {
-        var resetButton = document.getElementById('resetHistory');
-        if (this.items.length === 0) {
-            resetButton.disabled = true
-        } else {
-            resetButton.disabled = false
-        }
+        const resetButton = document.getElementById('resetHistory');
+        resetButton.disabled = this.items.length === 0;
     }
-    
+
     saveToLocalStorage() {
         localStorage.setItem('myQueue', JSON.stringify(this.items));
     }
-    
+
     dequeue() {
         return this.isEmpty() ? null : this.items.shift();
     }
@@ -45,17 +41,18 @@ class FixedSizeQueue {
     showSelectedIndex(i) {
         return this.isEmpty() ? null : this.items[i];
     }
-  
+
     isEmpty() {
-      return this.items.length === 0;
+        return this.items.length === 0;
     }
-  
+
     size() {
-      return this.items.length;
+        return this.items.length;
     }
 }
+
 const myQueue = new FixedSizeQueue();
-populateSidebar()
+populateSidebar();
 
 const editor = ace.edit("editor", {
     mode: "ace/mode/json",
@@ -66,135 +63,148 @@ const editor = ace.edit("editor", {
     cursorStyle: 'slim',
     highlightGutterLine: false,
     fontSize: 14
-}),
-    formatText = (e = 0) => {
-        try {
-            var t;
-            returnOriginalValue(), "" !== editor.getValue() ? (t = JSON.parse(editor.getValue()), (myQueue.enqueue(JSON.stringify(t,null, e))),populateSidebar(), editor.setValue(JSON.stringify(t,null, e)), editor.focus(), editor.selectAll(), 0 !== e && validState()) : alert("I think you are not clear about your goals.")
-        } catch (e) {
-            error()
+});
+
+function formatText(e = 0) {
+    try {
+        returnOriginalValue();
+        const value = editor.getValue();
+        if (value !== "") {
+            const parsedValue = JSON.parse(value);
+            myQueue.enqueue(JSON.stringify(parsedValue, null, e));
+            populateSidebar();
+            editor.setValue(JSON.stringify(parsedValue, null, e));
+            editor.focus();
+            editor.selectAll();
+            if (e !== 0) validState();
+        } else {
+            alert("I think you are not clear about your goals.");
         }
-    };
+    } catch (e) {
+        error();
+    }
+}
+
 editor.focus();
 editor.on("paste", e => {
     try {
-        returnOriginalValue(), e.text = JSON.stringify(JSON.parse(e.text), null, 4),validState()
+        returnOriginalValue();
+        e.text = JSON.stringify(JSON.parse(e.text), null, 4);
+        validState();
     } catch (e) {
-        error()
+        error();
     }
 });
-const clearText = () => {
-    try {
-        returnOriginalValue(), editor.setValue(null), editor.focus()
-    } catch (e) { }
-},
-    validState = () => {
-        document.getElementById("valid-state").style.display = "block"
-    },
-    error = () => {
-        document.getElementById("error").style.display = "block"
-    },
-    changeThemeToDark = () => {
-        document.documentElement.setAttribute("data-theme", "dark")
-        editor.setOptions({
-            theme: 'ace/theme/tomorrow_night'
-        })
-    },
-    resetLocalStorage = () => {
-        localStorage.removeItem('myQueue');
-        myQueue.loadFromLocalStorage()
-        var historyList = document.getElementById("historyList");
-        historyList.innerHTML = "";
-        setNoHistoryMessage()
-        closeHistoryPanel()
-    },
-    returnOriginalValue = () => {
-        document.getElementById("error").style.display = "none", document.getElementById("valid-state").style.display = "none"
-    };
-document.getElementById("minify").addEventListener("click", () => formatText()),
-document.getElementById("beautify").addEventListener("click", () => formatText(4)),
-document.getElementById("clear").addEventListener("click", () => clearText());
-document.getElementById("resetHistory").addEventListener("click", () => resetLocalStorage());
-window.addEventListener("beforeunload", function (e) {
-    var confirmationMessage = "\o/";
 
-    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    return confirmationMessage;                            //Webkit, Safari, Chrome
+function clearText() {
+    try {
+        returnOriginalValue();
+        editor.setValue(null);
+        editor.focus();
+    } catch (e) {}
+}
+
+function validState() {
+    document.getElementById("valid-state").style.display = "block";
+}
+
+function error() {
+    document.getElementById("error").style.display = "block";
+}
+
+function changeThemeToDark() {
+    document.documentElement.setAttribute("data-theme", "dark");
+    editor.setOptions({
+        theme: 'ace/theme/tomorrow_night'
+    });
+}
+
+function resetLocalStorage() {
+    localStorage.removeItem('myQueue');
+    myQueue.loadFromLocalStorage();
+    const historyList = document.getElementById("historyList");
+    historyList.innerHTML = "";
+    setNoHistoryMessage();
+    closeHistoryPanel();
+}
+
+function returnOriginalValue() {
+    document.getElementById("error").style.display = "none";
+    document.getElementById("valid-state").style.display = "none";
+}
+
+document.getElementById("minify").addEventListener("click", () => formatText());
+document.getElementById("beautify").addEventListener("click", () => formatText(4));
+document.getElementById("clear").addEventListener("click", clearText);
+document.getElementById("resetHistory").addEventListener("click", resetLocalStorage);
+
+window.addEventListener("beforeunload", function (e) {
+    const confirmationMessage = "\o/";
+    (e || window.event).returnValue = confirmationMessage;
+    return confirmationMessage;
 });
 
 function populateSidebar() {
-    var historyList = document.getElementById("historyList");
+    const historyList = document.getElementById("historyList");
     historyList.innerHTML = "";
 
     const savedQueue = localStorage.getItem('myQueue');
     const queueToShow = savedQueue ? JSON.parse(savedQueue) : [];
 
     if (queueToShow.length === 0) {
-        setNoHistoryMessage()
+        setNoHistoryMessage();
     } else {
-        // History records found, populate the sidebar
-        for (let i = 0; i < queueToShow.length; i++) {
-        var historyItem = document.createElement("li");
-        historyItem.textContent = queueToShow[i].data;
-        historyItem.classList.add("history-item");
+        for (const item of queueToShow) {
+            const historyItem = document.createElement("li");
+            historyItem.textContent = item.data;
+            historyItem.classList.add("history-item");
+            historyList.appendChild(historyItem);
 
-        // Append history item to the list
-        historyList.appendChild(historyItem);
-        // Create a list item for the date
-        var dateItem = document.createElement("li");
-        const constant = 'Saved Time'
-        const formattedDate = new Date(queueToShow[i].currentTime).toLocaleString()
-        dateItem.textContent = `${constant} - ${formattedDate}`;
-        dateItem.classList.add("history-date");
+            const dateItem = document.createElement("li");
+            const constant = 'Saved Time';
+            const formattedDate = new Date(item.currentTime).toLocaleString();
+            dateItem.textContent = `${constant} - ${formattedDate}`;
+            dateItem.classList.add("history-date");
+            historyList.appendChild(dateItem);
 
-        // Append date item to the list
-        historyList.appendChild(dateItem);
-
-            // Add click event listener
-            historyItem.addEventListener("click", function (item) {
-                return function () {
-                    handleClick(item);
-                };
-            }(queueToShow[i]));
+            historyItem.addEventListener("click", () => handleClick(item));
         }
     }
 }
 
-function setNoHistoryMessage () {
-    var historyList = document.getElementById("historyList");
+function setNoHistoryMessage() {
+    const historyList = document.getElementById("historyList");
     historyList.innerHTML = "";
-    // No history records found, display a message
-    var noHistoryMessage = document.createElement("li");
+    const noHistoryMessage = document.createElement("li");
     noHistoryMessage.textContent = "No history records found.";
     noHistoryMessage.classList.add("no-history-message");
     historyList.appendChild(noHistoryMessage);
 }
 
-
 function handleClick(item) {
-    editor.setValue(JSON.stringify(JSON.parse(item.data)))
-    formatText(4)
-    validState()
-    closeHistoryPanel()
+    editor.setValue(JSON.stringify(JSON.parse(item.data)));
+    formatText(4);
+    validState();
+    closeHistoryPanel();
 }
 
 function openHistoryPanel() {
-    var historyPanel = document.getElementById("historyPanel");
+    const historyPanel = document.getElementById("historyPanel");
     historyPanel.style.width = "350px";
     historyPanel.classList.add("show");
     document.getElementById("closeBtn").style.display = 'block';
 }
 
 function closeHistoryPanel() {
-    var historyPanel = document.getElementById("historyPanel");
+    const historyPanel = document.getElementById("historyPanel");
     historyPanel.style.width = "0";
     historyPanel.classList.remove("show");
     document.getElementById("closeBtn").style.display = 'none';
 }
 
 function toggleHistoryPanel(event) {
-    var historyPanel = document.getElementById("historyPanel");
-    
+    const historyPanel = document.getElementById("historyPanel");
+
     if (historyPanel.classList.contains("show")) {
         closeHistoryPanel();
     } else {
@@ -204,40 +214,35 @@ function toggleHistoryPanel(event) {
     event.stopPropagation();
 }
 
-var historyButton = document.getElementById("history");
+const historyButton = document.getElementById("history");
 historyButton.addEventListener("click", toggleHistoryPanel);
 
-var closeButton = document.getElementById("closeBtn");
-closeButton.addEventListener("click",closeHistoryPanel)
+const closeButton = document.getElementById("closeBtn");
+closeButton.addEventListener("click", closeHistoryPanel);
 
 document.addEventListener("click", function (event) {
-    var historyPanel = document.getElementById("historyPanel");
+    const historyPanel = document.getElementById("historyPanel");
 
     if (!historyPanel.contains(event.target) && historyPanel.classList.contains("show")) {
-        // Clicked outside the history panel, close it
         closeHistoryPanel();
     }
 });
 
-
 window.onbeforeunload = function (e) {
-    var message = "Are you sure ?";
-    var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
+    const message = "Are you sure ?";
+    const firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
     if (firefox) {
-        //Add custom dialog
-        //Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
-        var dialog = document.createElement("div");
+        const dialog = document.createElement("div");
         document.body.appendChild(dialog);
         dialog.id = "dialog";
         dialog.style.visibility = "hidden";
         dialog.innerHTML = message;
-        var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
+        const left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
         dialog.style.left = left + "px";
         dialog.style.visibility = "visible";
-        var shadow = document.createElement("div");
+        const shadow = document.createElement("div");
         document.body.appendChild(shadow);
         shadow.id = "shadow";
-        //tip with setTimeout
         setTimeout(function () {
             document.body.removeChild(document.getElementById("dialog"));
             document.body.removeChild(document.getElementById("shadow"));
